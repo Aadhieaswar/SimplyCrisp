d1d554763b510b6c8dec8ec20563f715aed2b541
aadhieaswar <aadhieaswar@gmail.com>
2022-05-25 23:03:49 -0400

    initial commit

--- src/Content/Purchase/Purchase.js
+++ src/Content/Purchase/Purchase.js
@@ -3,8 +3,16 @@ import React, { Component } from "react";
 export default class Purchase extends Component {
     render() {
         return (
-            <div className="content">
+            <div className="content text-center">
+                <img src="/boiled-peanut-salad.jpg" alt="boiled penut salad" width="300" />
 
+                <div className="blockquote h1">
+                Peanut Salsa
+                    <div className="item-cost">
+                        300g for $3.99
+                    </div>
+                </div>
+                
             </div>
         )
     }
